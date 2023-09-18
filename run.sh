2363dc42dbd8d784aa10fbb9593eb681ac19285f
XXTXTOP <xxtxtop@gmail.com>
2023-09-18 21:23:19 +0800

    add run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,14 @@ wget https://github.com/LinuxKernelBuilder/openkylin-docker/releases/download/V0
 
 sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
 sudo echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker --version
 
 sudo service docker start
 
