9cf5b158dfdc00f257e0e36c13be3accbfcecb20
XXTXTOP <xxtxtop@gmail.com>
2023-09-18 21:29:40 +0800

    add run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-sudo apt-get install ca-certificates  curl  gnupg  lsb-release wget
 wget https://github.com/LinuxKernelBuilder/openkylin-docker/releases/download/V0.1/yangtze-rootfs.tar.gz
 
 sudo mkdir -p /etc/apt/keyrings
@@ -7,7 +6,6 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmo
 
 sudo echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu bionic stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo docker --version
@@ -20,8 +18,8 @@ sudo docker run -it --name openkylin openkylin
 
 #以下应该是要在docker环境中进行操作的项目
 
-apt install neofetch
+sudo apt install neofetch
 
-neofetch
+sudo neofetch
 
-systemd-detect-virt -c
+sudo systemd-detect-virt -c
