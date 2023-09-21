4b86b0a7dc5409e62a8f4638c538f93047ca2e7a
XXTXTOP <xxtxtop@gmail.com>
2023-09-22 05:36:42 +0800

    add push docker hub1

--- 2.sh
+++ 2.sh
@@ -5,13 +5,13 @@ sudo docker run -dit openkylin:latest  /bin/bash
 sudo docker start openkylin
 sudo docker cp change_source.sh openkylin:/root
 sudo docker exec  openkylin /bin/bash /root/change_source.sh
-sudo docker exec openkylin vim --version /bin/bash
-sudo docker exec openkylin bash -c "sudo apt update && sudo apt upgrade"
-sudo docker exec openkylin bash -c "sudo apt install -y vim neofetch"
+#sudo docker exec openkylin vim --version /bin/bash
+#sudo docker exec openkylin bash -c "sudo apt update && sudo apt upgrade"
+#sudo docker exec openkylin bash -c "sudo apt install -y vim neofetch"
 sudo docker exec openkylin vim --version /bin/bash
 sudo docker exec openkylin neofetch --version /bin/bash
-sudo docker exec openkylin bash -c "sudo apt-get install  -y  git fakeroot build-essential libncurses-dev xz-utils libssl-dev bc flex libelf-dev bison"
-sudo docker exec openkylin bash -c "sudo apt install -y gpg python3-pyquery wget make gcc dpkg-dev python3-pip python3-tk "
+#sudo docker exec openkylin bash -c "sudo apt-get install  -y  git fakeroot build-essential libncurses-dev xz-utils libssl-dev bc flex libelf-dev bison"
+#sudo docker exec openkylin bash -c "sudo apt install -y gpg python3-pyquery wget make gcc dpkg-dev python3-pip python3-tk "
 
 
 docker commit openkylin openkylin:latest
