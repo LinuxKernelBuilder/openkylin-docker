5a9f9cd31d84f2a1eab78af52c1e24d3cc5df119
XXTXTOP <69441812+XXTX-TOP@users.noreply.github.com>
2023-11-11 22:01:32 +0800

    Update change_source.sh
    nile

--- change_source.sh
+++ change_source.sh
@@ -5,12 +5,12 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sudo truncate -s 0 /etc/apt/sources.list
 
 sudo cat >> /etc/apt/sources.list << EOF
-deb http://archive.build.openkylin.top/openkylin yangtze main
-deb http://archive.build.openkylin.top/OTHER/Internal-mirror/repack5/baseok0.fix yangtze main 
-deb http://archive.build.openkylin.top/OTHER/Internal-mirror/stage0 yangtze main
-deb http://archive.build.openkylin.top/openkylin yangtze-security main
-deb http://archive.build.openkylin.top/openkylin yangtze-updates main
-deb http://archive.build.openkylin.top/openkylin yangtze-proposed main
+deb http://archive.build.openkylin.top/openkylin nile main
+deb http://archive.build.openkylin.top/OTHER/Internal-mirror/repack5/baseok0.fix nile main 
+deb http://archive.build.openkylin.top/OTHER/Internal-mirror/stage0 nile main
+deb http://archive.build.openkylin.top/openkylin nile-security main
+deb http://archive.build.openkylin.top/openkylin nile-updates main
+deb http://archive.build.openkylin.top/openkylin nile-proposed main
 EOF
 
 sudo apt update && sudo apt upgrade
